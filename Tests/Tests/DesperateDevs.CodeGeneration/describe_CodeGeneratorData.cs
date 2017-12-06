425260605653e9ba82f0ded8b9a8f329540d1b91
Simon Schmid <mail@sschmid.com>
2017-12-07 00:00:48 +0100

    Added example data provider and generator

--- Tests/Tests/DesperateDevs.CodeGeneration/describe_CodeGeneratorData.cs
+++ Tests/Tests/DesperateDevs.CodeGeneration/describe_CodeGeneratorData.cs
@@ -5,10 +5,25 @@ class describe_CodeGeneratorData : nspec {
 
     void when_providing() {
 
+        CodeGeneratorData data = null;
+
+        before = () => {
+            data = new CodeGeneratorData();
+            data["Key"] = "World!";
+        };
+
         it["contains added object"] = () => {
-            var data = new CodeGeneratorData();
-            data["key"] = "value";
-            data["key"].should_be("value");
+            data["Key"].should_be("World!");
+        };
+
+        it["replaces placeholders"] = () => {
+            const string template = "Hello, ${Key}";
+            data.ReplacePlaceholders(template).should_be("Hello, World!");
+        };
+
+        it["respects case of placeholder"] = () => {
+            const string template = "Hello, ${key}";
+            data.ReplacePlaceholders(template).should_be("Hello, world!");
         };
     }
 }
