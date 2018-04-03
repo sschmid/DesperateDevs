a4a6c42e712279d96c38c935564f84d6b155c926
Simon Schmid <mail@sschmid.com>
2018-04-03 21:51:50 +0200

    Added EnsureStandalonePreProcessor

--- DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor/DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor/UnityCodeGenerator.cs
+++ DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor/DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor/UnityCodeGenerator.cs
@@ -14,53 +14,53 @@ namespace DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor {
 
         [MenuItem(CodeGeneratorMenuItems.generate, false, CodeGeneratorMenuItemPriorities.generate)]
         public static void Generate() {
-            checkCanGenerate();
+            if (checkCanGenerate()) {
+                Debug.Log("Generating...");
 
-            Debug.Log("Generating...");
+                var codeGenerator = CodeGeneratorUtil.CodeGeneratorFromPreferences(Preferences.sharedInstance);
 
-            var codeGenerator = CodeGeneratorUtil.CodeGeneratorFromPreferences(Preferences.sharedInstance);
+                var progressOffset = 0f;
 
-            var progressOffset = 0f;
+                codeGenerator.OnProgress += (title, info, progress) => {
+                    var cancel = EditorUtility.DisplayCancelableProgressBar(title, info, progressOffset + progress / 2);
+                    if (cancel) {
+                        codeGenerator.Cancel();
+                    }
+                };
 
-            codeGenerator.OnProgress += (title, info, progress) => {
-                var cancel = EditorUtility.DisplayCancelableProgressBar(title, info, progressOffset + progress / 2);
-                if (cancel) {
-                    codeGenerator.Cancel();
+                CodeGenFile[] dryFiles = null;
+                CodeGenFile[] files = null;
+
+                try {
+                    dryFiles = codeGenerator.DryRun();
+                    progressOffset = 0.5f;
+                    files = codeGenerator.Generate();
+                } catch (Exception ex) {
+                    dryFiles = new CodeGenFile[0];
+                    files = new CodeGenFile[0];
+
+                    EditorUtility.DisplayDialog("Error", ex.Message, "Ok");
                 }
-            };
 
-            CodeGenFile[] dryFiles = null;
-            CodeGenFile[] files = null;
+                EditorUtility.ClearProgressBar();
 
-            try {
-                dryFiles = codeGenerator.DryRun();
-                progressOffset = 0.5f;
-                files = codeGenerator.Generate();
-            } catch (Exception ex) {
-                dryFiles = new CodeGenFile[0];
-                files = new CodeGenFile[0];
+                var totalGeneratedFiles = files.Select(file => file.fileName).Distinct().Count();
 
-                EditorUtility.DisplayDialog("Error", ex.Message, "Ok");
+                var sloc = dryFiles
+                    .Select(file => file.fileContent.ToUnixLineEndings())
+                    .Sum(content => content.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length);
+
+                var loc = files
+                    .Select(file => file.fileContent.ToUnixLineEndings())
+                    .Sum(content => content.Split(new[] { '\n' }).Length);
+
+                Debug.Log("Generated " + totalGeneratedFiles + " files (" + sloc + " sloc, " + loc + " loc)");
+
+                AssetDatabase.Refresh();
             }
-
-            EditorUtility.ClearProgressBar();
-
-            var totalGeneratedFiles = files.Select(file => file.fileName).Distinct().Count();
-
-            var sloc = dryFiles
-                .Select(file => file.fileContent.ToUnixLineEndings())
-                .Sum(content => content.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length);
-
-            var loc = files
-                .Select(file => file.fileContent.ToUnixLineEndings())
-                .Sum(content => content.Split(new[] { '\n' }).Length);
-
-            Debug.Log("Generated " + totalGeneratedFiles + " files (" + sloc + " sloc, " + loc + " loc)");
-
-            AssetDatabase.Refresh();
         }
 
-        static void checkCanGenerate() {
+        static bool checkCanGenerate() {
             if (EditorApplication.isCompiling) {
                 throw new Exception("Cannot generate because Unity is still compiling. Please wait...");
             }
@@ -74,11 +74,14 @@ namespace DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor {
             var canCompile = (int)logEntries.GetMethod("GetCount").Invoke(new object(), null) == 0;
             if (!canCompile) {
                 Debug.Log("There are compile errors! Generated code will be based on last compiled executable.");
-                EditorUtility.DisplayDialog("Jenny",
+                return EditorUtility.DisplayDialog("Jenny",
                     "There are compile errors! Generated code will be based on last compiled executable.",
-                    "Ok"
+                    "Ok",
+                    "Cancel"
                 );
             }
+
+            return true;
         }
 
         static string _propertiesPath;
---
+++ DesperateDevs.CodeGeneration.Plugins/DesperateDevs.CodeGeneration.Plugins/PreProcessors/EnsureStandalonePreProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using DesperateDevs.Utils;
+
+namespace DesperateDevs.CodeGeneration.Plugins {
+
+    public class EnsureStandalonePreProcessor : IPreProcessor {
+
+        public string name { get { return "Ensure Standalone"; } }
+        public int priority { get { return -5; } }
+        public bool runInDryMode { get { return true; } }
+
+        public void PreProcess() {
+            var types = AppDomain.CurrentDomain.GetAllTypes();
+            var isStandalone = types.Any(type => type.FullName == "DesperateDevs.CodeGeneration.CodeGenerator.CLI.Program");
+            if (!isStandalone) {
+                throw new Exception("Jenny is configured to only work with the standalone code generator!");
+            }
+        }
+    }
+}
