dbbfc2272906f3a2a74e0cced3e51bd2c985f221
Simon Schmid <mail@sschmid.com>
2021-09-02 21:50:30 +0200

    Move all projects into src folder and start migrating to new project structure

---
+++ src/DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor/src/CodeGeneratorMenuItems.cs
@@ -0,0 +1,16 @@
+﻿namespace DesperateDevs.CodeGeneration.CodeGenerator.Unity.Editor
+{
+    public static class CodeGeneratorMenuItems
+    {
+        public const string preferences     = "Tools/Jenny/Preferences... #%j";
+        public const string generate        = "Tools/Jenny/Generate #%g";
+        public const string generate_server = "Tools/Jenny/Generate with Server %&g";
+    }
+
+    public static class CodeGeneratorMenuItemPriorities
+    {
+        public const int preferences     = 1;
+        public const int generate        = 2;
+        public const int generate_server = 3;
+    }
+}
