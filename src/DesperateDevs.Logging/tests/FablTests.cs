74a726f36728041af3f57c864210399ed8868983
Simon Schmid <mail@sschmid.com>
2021-09-06 21:45:51 +0200

    Migrate DesperateDevs.Logging.Tests

---
+++ src/DesperateDevs.Logging/tests/FablTests.cs
@@ -0,0 +1,126 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DesperateDevs.Logging.Tests
+{
+    public class FablTests
+    {
+        public FablTests()
+        {
+            fabl.ResetLoggers();
+            fabl.ResetAppenders();
+            fabl.globalLogLevel = LogLevel.On;
+        }
+
+        [Fact]
+        public void CreatesNewLogger()
+        {
+            var logger = fabl.GetLogger("MyLogger");
+            logger.Should().NotBeNull();
+            logger.GetType().Should().BeSameAs(typeof(Logger));
+            logger.name.Should().Be("MyLogger");
+            logger.logLevel.Should().Be(LogLevel.On);
+        }
+
+        [Fact]
+        public void ReturnsSameLoggerWhenNameIsEqual()
+        {
+            var logger1 = fabl.GetLogger("MyLogger");
+            var logger2 = fabl.GetLogger("MyLogger");
+            logger1.Should().BeSameAs(logger2);
+        }
+
+        [Fact]
+        public void ClearsCreatedLoggers()
+        {
+            var logger1 = fabl.GetLogger("MyLogger");
+            fabl.ResetLoggers();
+            var logger2 = fabl.GetLogger("MyLogger");
+            logger1.Should().NotBeSameAs(logger2);
+        }
+
+        [Fact]
+        public void CreatesNewLoggerWithGlobalLogLevel()
+        {
+            fabl.globalLogLevel = LogLevel.Error;
+            var logger = fabl.GetLogger("MyLogger");
+            logger.logLevel.Should().Be(LogLevel.Error);
+        }
+
+        [Fact]
+        public void SetsGlobalLogLevelOnCreatedLogger()
+        {
+            var logger = fabl.GetLogger("MyLogger");
+            logger.logLevel.Should().Be(LogLevel.On);
+            fabl.globalLogLevel = LogLevel.Error;
+            logger.logLevel.Should().Be(LogLevel.Error);
+        }
+
+        [Fact]
+        public void CreatesNewLoggerWithGlobalAppender()
+        {
+            var appenderLogLevel = LogLevel.Off;
+            var appenderMessage = string.Empty;
+            fabl.AddAppender((log, logLevel, message) =>
+            {
+                appenderLogLevel = logLevel;
+                appenderMessage = message;
+            });
+
+            var appenderLogLevel2 = LogLevel.Off;
+            var appenderMessage2 = string.Empty;
+            fabl.AddAppender((log, logLevel, message) =>
+            {
+                appenderLogLevel2 = logLevel;
+                appenderMessage2 = message;
+            });
+
+            var logger = fabl.GetLogger("MyLogger");
+            logger.Info("hi");
+
+            appenderLogLevel.Should().Be(LogLevel.Info);
+            appenderMessage.Should().Be("hi");
+            appenderLogLevel2.Should().Be(LogLevel.Info);
+            appenderMessage2.Should().Be("hi");
+        }
+
+        [Fact]
+        public void AddsAppenderOnCreatedLogger()
+        {
+            var logger = fabl.GetLogger("MyLogger");
+            var didLog = false;
+            fabl.AddAppender((log, logLevel, message) => didLog = true);
+            logger.Info("hi");
+            didLog.Should().BeTrue();
+        }
+
+        [Fact]
+        public void RemovesAppenderOnCreatedLogger()
+        {
+            var didLog = false;
+            LogDelegate appender = (log, logLevel, message) => didLog = true;
+            fabl.AddAppender(appender);
+            var logger = fabl.GetLogger("MyLogger");
+            fabl.RemoveAppender(appender);
+            logger.Info("hi");
+            didLog.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ClearsGlobalAppenders()
+        {
+            var appenderLogLevel = LogLevel.Off;
+            var appenderMessage = string.Empty;
+            fabl.AddAppender((log, logLevel, message) =>
+            {
+                appenderLogLevel = logLevel;
+                appenderMessage = message;
+            });
+            fabl.ResetAppenders();
+            var logger = fabl.GetLogger("MyLogger");
+            logger.Info("hi");
+            appenderLogLevel.Should().Be(LogLevel.Off);
+            appenderMessage.Should().Be(string.Empty);
+        }
+    }
+}
---
+++ src/DesperateDevs.Logging/tests/LoggerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace DesperateDevs.Logging.Tests
+{
+    public class LoggerTests
+    {
+        const string message = "hi";
+        const string loggerName = "MyLogger";
+
+        readonly Logger _logger;
+
+        public LoggerTests()
+        {
+            _logger = new Logger("MyLogger");
+        }
+
+        [Theory]
+        [InlineData(LogLevel.On, true, true, true, true, true, true)]
+        [InlineData(LogLevel.Trace, true, true, true, true, true, true)]
+        [InlineData(LogLevel.Debug, false, true, true, true, true, true)]
+        [InlineData(LogLevel.Info, false, false, true, true, true, true)]
+        [InlineData(LogLevel.Warn, false, false, false, true, true, true)]
+        [InlineData(LogLevel.Error, false, false, false, false, true, true)]
+        [InlineData(LogLevel.Fatal, false, false, false, false, false, true)]
+        [InlineData(LogLevel.Off, false, false, false, false, false, false)]
+        public void LogLevels(LogLevel logLevel, bool trace, bool debug, bool info, bool warn, bool error, bool fatal)
+        {
+            _logger.logLevel = logLevel;
+            AssertLogLevel(_logger.Trace, LogLevel.Trace, trace);
+            AssertLogLevel(_logger.Debug, LogLevel.Debug, debug);
+            AssertLogLevel(_logger.Info, LogLevel.Info, info);
+            AssertLogLevel(_logger.Warn, LogLevel.Warn, warn);
+            AssertLogLevel(_logger.Error, LogLevel.Error, error);
+            AssertLogLevel(_logger.Fatal, LogLevel.Fatal, fatal);
+
+            void AssertLogLevel(Action<string> logMethod, LogLevel logLvl, bool shouldLog)
+            {
+                var didLog = false;
+                var eventLogLevel = LogLevel.Off;
+                string eventMessage = null;
+                Logger eventLogger = null;
+                _logger.OnLog += (logger, logLevel, msg) =>
+                {
+                    didLog = true;
+                    eventLogger = logger;
+                    eventLogLevel = logLevel;
+                    eventMessage = msg;
+                };
+
+                logMethod(message);
+
+                didLog.Should().Be(shouldLog);
+
+                if (shouldLog)
+                {
+                    eventLogger.Should().BeSameAs(_logger);
+                    eventMessage.Should().Be(message);
+                    eventLogLevel.Should().Be(logLvl);
+                }
+                else
+                {
+                    eventMessage.Should().BeNull();
+                    eventLogLevel.Should().Be(LogLevel.Off);
+                    eventLogger.Should().BeNull();
+                }
+            }
+        }
+
+        [Fact]
+        public void AssertDoesNotThrowWhenConditionIsTrue()
+        {
+            _logger.Assert(true, "success");
+        }
+
+        [Fact]
+        public void AssertThrowsWhenConditionIsFalse()
+        {
+            _logger.Invoking(logger => logger.Assert(false, "fail"))
+                .Should().Throw<FablAssertException>();
+        }
+
+        [Fact]
+        public void ResetsOnLog()
+        {
+            var didLog = 0;
+            _logger.OnLog += (logger, level, s) => didLog += 1;
+            _logger.Reset();
+            _logger.Info("Test");
+            didLog.Should().Be(0);
+        }
+    }
+}
