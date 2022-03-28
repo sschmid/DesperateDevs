d7b3028f0b1836129a90065e241c49363d29a78c
Simon Schmid <mail@sschmid.com>
2022-03-28 12:38:26 +0200

    Benchmark Properties creation

--- src/DesperateDevs.Serialization/benchmarks/PropertiesBenchmarks.cs
+++ src/DesperateDevs.Serialization/benchmarks/PropertiesBenchmarks.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 
@@ -12,6 +12,27 @@ namespace DesperateDevs.Serialization.Benchmarks
         [Params(100, 10000)]
         public int Lines { get; set; }
 
+        string _input;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < Lines; i++)
+            {
+                var value = i.ToString();
+                sb.Append("key");
+                sb.Append(value);
+                sb.Append(" = value");
+                sb.AppendLine(value);
+            }
+
+            _input = sb.ToString();
+        }
+
+        [Benchmark]
+        public Properties CreateProperties() => new Properties(_input);
+
         [Benchmark]
         public Properties AddProperties()
         {
@@ -23,38 +44,19 @@ namespace DesperateDevs.Serialization.Benchmarks
         }
 
         [Benchmark]
-        public Properties GetProperties()
+        public void GetProperties()
         {
-            var properties = new Properties();
-            for (var i = 0; i < Lines; i++)
-                properties["key" + i] = "value" + i;
-
+            var properties = new Properties(_input);
             for (var i = 0; i < Lines; i++)
             {
                 var value = properties["key" + i];
             }
-
-            return properties;
         }
 
         [Benchmark]
-        public string PropertiesToString()
-        {
-            var properties = new Properties();
-            for (var i = 0; i < Lines; i++)
-                properties["key" + i] = "value" + i;
-
-            return properties.ToString();
-        }
+        public string PropertiesToString() => new Properties(_input).ToString();
 
         [Benchmark]
-        public string PropertiesToMinifiedString()
-        {
-            var properties = new Properties();
-            for (var i = 0; i < Lines; i++)
-                properties["key" + i] = "value" + i;
-
-            return properties.ToMinifiedString();
-        }
+        public string PropertiesToMinifiedString() => new Properties(_input).ToMinifiedString();
     }
 }
