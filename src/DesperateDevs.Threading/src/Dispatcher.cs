735b9a4c263e7ebf4244d73f38546a5ad42e3400
Simon Schmid <mail@sschmid.com>
2022-04-12 22:37:31 +0200

    Format DesperateDevs.Threading

--- src/DesperateDevs.Threading/src/Dispatcher.cs
+++ src/DesperateDevs.Threading/src/Dispatcher.cs
@@ -2,45 +2,61 @@
 using System.Collections.Generic;
 using System.Threading;
 
-namespace DesperateDevs.Threading {
-
-    public class Dispatcher {
-
-        public bool isOnThread { get { return _threadId == Thread.CurrentThread.ManagedThreadId; } }
+namespace DesperateDevs.Threading
+{
+    public class Dispatcher
+    {
+        public bool isOnThread
+        {
+            get { return _threadId == Thread.CurrentThread.ManagedThreadId; }
+        }
 
         readonly int _threadId;
         readonly List<Action> _actions;
         readonly object _lock = new object();
 
-        public Dispatcher(Thread thread) {
+        public Dispatcher(Thread thread)
+        {
             _threadId = thread.ManagedThreadId;
-            lock (_lock) {
+            lock (_lock)
+            {
                 _actions = new List<Action>();
             }
         }
 
-        public void Queue(Action action) {
-            if (isOnThread) {
+        public void Queue(Action action)
+        {
+            if (isOnThread)
+            {
                 action();
-            } else {
-                lock (_lock) {
+            }
+            else
+            {
+                lock (_lock)
+                {
                     _actions.Add(action);
                 }
             }
         }
 
-        public void Run() {
-            if (isOnThread) {
+        public void Run()
+        {
+            if (isOnThread)
+            {
                 Action[] actions = null;
-                lock (_lock) {
-                    if (_actions.Count > 0) {
+                lock (_lock)
+                {
+                    if (_actions.Count > 0)
+                    {
                         actions = _actions.ToArray();
                         _actions.Clear();
                     }
                 }
 
-                if (actions != null) {
-                    foreach (var action in actions) {
+                if (actions != null)
+                {
+                    foreach (var action in actions)
+                    {
                         action();
                     }
                 }
