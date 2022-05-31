7c2ba3385e3fb89bd5901d0b4e775cc16732b172
Simon Schmid <mail@sschmid.com>
2022-05-31 18:50:10 +0200

    Refactor TargetFrameworkProfilePreProcessorTests

--- src/Jenny.Plugins/tests/TargetFrameworkProfilePreProcessorTests.cs
+++ src/Jenny.Plugins/tests/TargetFrameworkProfilePreProcessorTests.cs
@@ -14,32 +14,23 @@ namespace Jenny.Plugins.Tests
         [Fact]
         public void RemovesUnitySubsetv35()
         {
-            var temp = Path.Combine(FixturesPath, "temp");
-            if (!Directory.Exists(temp))
-                Directory.CreateDirectory(temp);
-
-            var project = Path.Combine(FixturesPath, "UnitySubsetv35.csproj");
-            var tempProject = Path.Combine(temp, "UnitySubsetv35.csproj");
-
-            File.Copy(project, tempProject, true);
-
-            var preProcessor = new TargetFrameworkProfilePreProcessor();
-            var preferences = new TestPreferences($"Jenny.Plugins.ProjectPath = {tempProject}");
-            preProcessor.Configure(preferences);
-            preProcessor.PreProcess();
-
-            File.ReadAllText(tempProject).Should().NotContain("Unity Subset v3.5");
+            RemoveTargetFrameworkProfile("UnitySubsetv35.csproj").Should().NotContain("Unity Subset v3.5");
         }
 
         [Fact]
         public void RemovesUnityFullv35()
         {
+            RemoveTargetFrameworkProfile("UnityFullv35.csproj").Should().NotContain("Unity Full v3.5");
+        }
+
+        string RemoveTargetFrameworkProfile(string csproj)
+        {
             var temp = Path.Combine(FixturesPath, "temp");
             if (!Directory.Exists(temp))
                 Directory.CreateDirectory(temp);
 
-            var project = Path.Combine(FixturesPath, "UnityFullv35.csproj");
-            var tempProject = Path.Combine(temp, "UnityFullv35.csproj");
+            var project = Path.Combine(FixturesPath, csproj);
+            var tempProject = Path.Combine(temp, csproj);
 
             File.Copy(project, tempProject, true);
 
@@ -47,8 +38,7 @@ namespace Jenny.Plugins.Tests
             var preferences = new TestPreferences($"Jenny.Plugins.ProjectPath = {tempProject}");
             preProcessor.Configure(preferences);
             preProcessor.PreProcess();
-
-            File.ReadAllText(tempProject).Should().NotContain("Unity Full v3.5");
+            return File.ReadAllText(tempProject);
         }
     }
 }
