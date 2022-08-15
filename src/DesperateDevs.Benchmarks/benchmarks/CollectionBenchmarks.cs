58d0683e346ab679dc9e4fe440913b9984ff0e6c
Simon Schmid <mail@sschmid.com>
2022-08-15 15:25:59 +0200

    Add DesperateDevs.Benchmarks

---
+++ src/DesperateDevs.Benchmarks/benchmarks/CollectionBenchmarks.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace DesperateDevs.Benchmarks
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class CollectionBenchmarks
+    {
+        [Params(100, 10000)]
+        public int N { get; set; }
+
+        string[] _values;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _values = new string[N];
+            for (var i = 0; i < N; i++) _values[i] = i.ToString();
+        }
+
+        [Benchmark]
+        public void ReturnAsArray()
+        {
+            foreach (var i in GetAsArray()) { }
+        }
+
+        [Benchmark]
+        public void ReturnAsIEnumerable()
+        {
+            foreach (var i in GetAsIEnumerable()) { }
+        }
+
+        [Benchmark]
+        public void ReturnAsIEnumerableToArray()
+        {
+            foreach (var i in GetAsIEnumerable().ToArray()) { }
+        }
+
+        string[] GetAsArray() => _values;
+        IEnumerable<string> GetAsIEnumerable() => _values;
+    }
+}
