a510dbf108ec86e128b97e3c00adeed7a642ecd3
Simon Schmid <mail@sschmid.com>
2022-08-26 13:47:23 +0200

    Remove Sherlog from DesperateDevs.Roslyn

--- src/DesperateDevs.Roslyn/src/ProjectParser.cs
+++ src/DesperateDevs.Roslyn/src/ProjectParser.cs
@@ -2,26 +2,20 @@
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
-using Sherlog;
 
 namespace DesperateDevs.Roslyn
 {
     public class ProjectParser
     {
-        static readonly Logger _logger = Logger.GetLogger(nameof(ProjectParser));
-
         readonly Project _project;
         INamedTypeSymbol[] _types;
 
         public ProjectParser(string projectPath)
         {
-            _logger.Debug("Opening " + projectPath);
-
             if (!MSBuildLocator.IsRegistered) MSBuildLocator.RegisterDefaults();
             using (var workspace = MSBuildWorkspace.Create())
             {
                 _project = workspace.OpenProjectAsync(projectPath).Result;
-                _logger.Debug("Opened " + _project.Name);
             }
         }
 
@@ -29,14 +23,10 @@ namespace DesperateDevs.Roslyn
         {
             if (_types == null)
             {
-                _logger.Debug("Parsing " + _project.Name);
-
                 _types = _project.GetCompilationAsync().Result
                     .GetSymbolsWithName(name => true, SymbolFilter.Type)
                     .OfType<INamedTypeSymbol>()
                     .ToArray();
-
-                _logger.Debug("Complete " + _project.Name);
             }
 
             return _types;
